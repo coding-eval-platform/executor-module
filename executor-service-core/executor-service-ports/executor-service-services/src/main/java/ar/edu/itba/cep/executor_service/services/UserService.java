0ff415061a9d10a60640d884941d75431f8fdcf7
Juan Marcos Bellini <juanmbellini@gmail.com>
2019-04-12 00:33:41 -0300

    First commit
    - Set up project
    Closes #10

---
+++ executor-service-core/executor-service-ports/executor-service-services/src/main/java/ar/edu/itba/cep/executor_service/services/UserService.java
@@ -0,0 +1,9 @@
+package ar.edu.itba.cep.executor_service.services;
+
+import ar.edu.itba.cep.executor_service.models.User;
+
+/**
+ * A port into the application that allows {@link User} management.
+ */
+public interface UserService {
+}
