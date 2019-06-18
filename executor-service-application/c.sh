5360e45bc3fc7ddd70029e0a071a95a909944a74
Juan Marcos Bellini <juanmbellini@gmail.com>
2019-06-17 23:56:01 -0300

    Improve all the executors
    - Move the shebang line to the top
    - Change the initialization phase: Now it uses the here-string redirection as escapable characters may appear in the code
    - Implement ruby and C executors

--- executor-service-application/c.sh
+++ executor-service-application/c.sh
@@ -1,19 +1,18 @@
+#!/bin/bash
 # Copyright 2018-2019 Bellini & Lobo
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-#!/bin/bash
 
 # This is a template used to create new executors
 # Note that the script will be called setting the CODE and TIMEOUT global variables,
@@ -25,15 +24,13 @@
 
 set -e # Stop script execution if something goes wrong
 
-echo $CODE
-echo $TIMEOUT
-echo $@
+# Initialization
+cat <<< "$CODE" > ./main.c
 
-# First, initialize stuff if necessary
-# For example, echo $CODE > ./Main.java
+# Compilation
+gcc ./main.c -o main # > /dev/null 2> ./compile-errors
 
-# Then compile te code if necessary
-# javac ./Main.java > /dev/null
+# TODO: check error code and compile errors
 
-# Finally run the code passing all arguments with the "$@" variable
-# timeout $TIMEOUT java Main "$@"
+# Execution
+timeout "$TIMEOUT" ./main "$@"
